04a25f062edb091dbcd7afb1cb8118fd1c2bf578
Krtucho <krtucho98@gmail.com>
2021-06-11 15:00:11 -0400

    best

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatStepperModule} from '@angular/material/stepper';
+import { ProfileComponent } from './profile/profile.component';
+import { ToolbarComponent } from './profile/toolbar/toolbar.component';
+import { InfoComponent } from './profile/info/info.component';
+import { OptionsComponent } from './profile/options/options.component';
+import { OpinionsComponent } from './profile/opinions/opinions.component';
+import { OffersComponent } from './profile/offers/offers.component';
+import { StepsComponent } from './profile/steps/steps.component';
+import { ReservationCardComponent } from './profile/reservation-card/reservation-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    ProfileComponent,
+    ToolbarComponent,
+    InfoComponent,
+    OptionsComponent,
+    OpinionsComponent,
+    OffersComponent,
+    StepsComponent,
+    ReservationCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
