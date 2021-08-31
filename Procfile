e2cd19ca11d61ca402009018f5ee87311f6aa30d
Krtucho <Krtucho98@gmail.com>
2021-08-31 19:38:57 -0400

    changed npm to node sever

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: npm start
+web: node server.js
 // this enable to start without using ts-node
 // this will run prestart:prod automatically
 // "start": "ng serve", // Uncomment this line for local build
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'src/index.html'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'src/index.html'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/my-app'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/my-app/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
