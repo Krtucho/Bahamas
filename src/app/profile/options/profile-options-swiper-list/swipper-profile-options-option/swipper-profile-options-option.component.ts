def26b4e825570e05f3101ac2cd4d2db9b712377
Krtucho <Krtucho98@gmail.com>
2021-08-31 21:02:27 -0400

    heroku server changes

--- src/app/profile/options/profile-options-swiper-list/swipper-profile-options-option/swipper-profile-options-option.component.ts
+++ src/app/profile/options/profile-options-swiper-list/swipper-profile-options-option/swipper-profile-options-option.component.ts
@@ -10,9 +10,9 @@ SwiperCore.use([Navigation]);
 
 @Component({
   selector: 'app-swipper-profile-options-option',
-  template: `<swiper fxFlex="100%" [navigation]="false" [freeMode]="true">
+  template: `<swiper [navigation]="false" [freeMode]="true">
   <ng-template swiperSlide>
-    <app-profile-options-option style="background-color: grey; color: white; margin-left: 20px; max-width: 380px;"></app-profile-options-option>
+    <app-profile-options-option style="background-color: grey; color: white; paddong: 20px;"></app-profile-options-option>
   </ng-template>
   <ng-template swiperSlide>
     <app-profile-options-option style="background-color: grey; color: white; margin-left: 20px; max-width: 380px;" ></app-profile-options-option>
